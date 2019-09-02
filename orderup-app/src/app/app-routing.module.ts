1e347cf7163ec9ac169b97603d0b71047f1bcd05
hardin253874 <hardin.253874@gmail.com>
2019-09-02 21:54:11 +1000

    Add files via upload

--- orderup-app/src/app/app-routing.module.ts
+++ orderup-app/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './products/product-details.component';
+import { CartComponent } from './products/cart.component';
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  { path: '', component: HomeComponent},
   { path: 'products/:imageId', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
--- orderup-app/src/app/app.module.ts
+++ orderup-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormBuilder ,FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import {SlideshowModule} from 'ng-simple-slideshow';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,20 +8,28 @@ import { HomeComponent } from './home/home.component';
 import { HomeService} from './home/home.service';
 import { ProductAlertsComponent} from './products/product-alerts.component';
 import {ProductDetailsComponent} from './products/product-details.component';
+import { CartService } from './products/cart.service';
+import { CartComponent } from './products/cart.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlideshowModule
+    SlideshowModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
-    HomeService
+    HomeService,
+    CartService,
+    FormBuilder
   ],
   bootstrap: [AppComponent]
 })
